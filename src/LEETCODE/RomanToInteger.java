cf09954999db69d40b7bb47794d997ff80273ded
alexKamo <sashakamor@gmail.com>
2024-06-24 19:52:56 -0500

    JavaCourses

--- src/LEETCODE/RomanToInteger.java
+++ src/LEETCODE/RomanToInteger.java
@@ -1,31 +1,30 @@
 package LEETCODE;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class RomanToInteger {
 
-    public static int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+    public static int romanToInt (String s){
+        Map<Character, Integer> roman = new HashMap<>();
 
-        int res = 0;
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-
-            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
-                res -= map.get(s.charAt(i));
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+        int sum = 0;
+        for (int i = 0;i<s.length()-1;i++) {
+            if ( roman.get(s.charAt(i)) >=  roman.get(s.charAt(i + 1))) {
+                sum +=  roman.get(s.charAt(i));
             } else {
-                res += map.get(s.charAt(i));
+                sum -=  roman.get(s.charAt(i));
             }
-            res += map.get(s.charAt(s.length() - 1));
         }
-        return res;
+        sum += roman.get(s.charAt(s.length()-1));
+        return sum;
     }
 
     public static void main(String[] args) {
--- src/LEETCODE/validAnagram.java
+++ src/LEETCODE/validAnagram.java
@@ -10,11 +10,14 @@ public class validAnagram {
         Map<Character, Integer> map = new HashMap<>();
 
         for(int i = 0; i<s.length(); i++){
-            char letter = s.charAt(i);
-            if(map.containsKey(letter)){
-                map.put(letter,map.get(letter)+1);
-            }
-            else map.put(letter,1);
+            char letter1 = s.charAt(i);
+            if(map.containsKey(letter1)){
+                map.put(letter1,map.get(letter1)+1);
+            }else map.put(letter1,1);
+
+            char letter2 = t.charAt(i);
+            if (map.containsKey(letter2)) map.put(letter2, map.get(letter1)-1);
+            else map.put(letter2,1);
         }
         return true;
     }
