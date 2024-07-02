87cbed2ae755c51324e4623bcca3ee694d429b07
alexKamo <sashakamor@gmail.com>
2024-07-01 19:31:15 -0500

    JavaCourses

--- src/LEETCODE/LongestNiceSubstring.java
+++ src/LEETCODE/LongestNiceSubstring.java
@@ -11,7 +11,8 @@ public class LongestNiceSubstring {
         for (int i = 0; i < s.length(); i++) set.add(s.charAt(i));
 
         for (int i = 0; i < s.length(); i++) {
-            if (set.contains(Character.toUpperCase(s.charAt(i))) && set.contains(Character.toLowerCase(s.charAt(i))))
+            if (set.contains(Character.toUpperCase(s.charAt(i)))
+                    && set.contains(Character.toLowerCase(s.charAt(i))))
                 continue;
             String s1 = longestNiceSubstring(s.substring(0,i));
             String s2 = longestNiceSubstring(s.substring(i+1));
