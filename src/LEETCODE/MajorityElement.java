2838f0df1cb12eeaa1f1dbe319a55fa2341fbe13
alexKamo <sashakamor@gmail.com>
2024-07-01 20:11:23 -0500

    JavaCourses

---
+++ src/LEETCODE/MajorityElement.java
@@ -0,0 +1,19 @@
+package LEETCODE;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums){
+        int majEl = 0;
+        int count = 0;
+
+        for(int num : nums){
+            if (count == 0){
+                majEl = num;
+            }
+            if (num == majEl){
+                count++;
+            }else count--;
+        }
+
+        return majEl;
+    }
+}
