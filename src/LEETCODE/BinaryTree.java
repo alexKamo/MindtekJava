2c608fb25673c2ae97b89e3797b8e869681fd6ae
alexKamo <sashakamor@gmail.com>
2024-07-08 18:52:29 -0500

    JavaCourses

---
+++ src/LEETCODE/BinaryTree.java
@@ -0,0 +1,24 @@
+package LEETCODE;
+
+import LEETCODE.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTree {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> list = new ArrayList<>();
+        inorderTraversal(root,list);
+        return list;
+
+    }
+
+    void inorderTraversal(TreeNode root, List<Integer> list){
+        if (root == null) return;
+        inorderTraversal(root.left, list);
+        list.add(root.val);
+        inorderTraversal(root.right, list);
+    }
+
+}
