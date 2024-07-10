18bb4832638c6907b6f34e615a4fdac4da956c86
alexKamo <sashakamor@gmail.com>
2024-07-09 19:52:51 -0500

    JavaCourses

--- src/LEETCODE/CountNodesTree.java
+++ src/LEETCODE/CountNodesTree.java
@@ -1,5 +1,8 @@
 package LEETCODE;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CountNodesTree {
 
     public int countNodes(TreeNode root) {
@@ -7,6 +10,22 @@ public class CountNodesTree {
         if (root == null) return 0;
         return 1+ countNodes(root.left) + countNodes(root.right);
 
+    }
+
+    // second solution
+    public int countNodes2(TreeNode root) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        int count = 0;
+        if(root != null) queue.add(root);
+
+        while (!queue.isEmpty()){
+            TreeNode temp = queue.poll();
+            count++;
+            if (temp.left != null) queue.add(temp.left);
+            if (temp.right != null) queue.add(temp.right);
+        }
+        return  count;
     }
 
 }
