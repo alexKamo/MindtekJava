fc9231119f6eec6de3cc09c8cdd6e6c5cbde9b9d
alexKamo <sashakamor@gmail.com>
2024-07-11 18:50:19 -0500

    JavaCourses

--- src/multithreading/Intro.java
+++ src/multithreading/Intro.java
@@ -21,14 +21,43 @@ public class Intro {
             System.out.println("main1 -> " + i);
         }
 
-    }
 
-    /*
+            /*
      Ways to create threads objects:
      1 extends Thread class
      2 implement Runnable interface
      3 Anonymous implementation
      4 Lambda function
      */
+
+        // Anonymous implementation
+        Runnable runNable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Anonymous implementation");
+            }
+
+        // runNable.run(); -> run method executed like a standard method
+        };
+
+        Thread thrObject1 = new Thread(runNable);
+        thrObject1.start();
+
+
+        // LAMBDA EXP
+        Runnable runnable = () -> { System.out.println("lambda");};
+        runnable.run();
+
+        Thread thrObject2 = new Thread(runnable);
+        thrObject2.start();
+
+
+
+
+
+
+    }
+
+
 
 }
