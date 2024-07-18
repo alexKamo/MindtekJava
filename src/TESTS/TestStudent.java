946c8f9d2728cb1da39de0487baeabc3db10e0c6
alexKamo <sashakamor@gmail.com>
2024-07-18 17:19:42 -0500

    JavaCourses

---
+++ src/TESTS/TestStudent.java
@@ -0,0 +1,26 @@
+package TESTS;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestStudent {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student(105,"John","New York");
+        Student s2 = new Student(102,"Patel","Chicago");
+        Student s3 = new Student(106,"Kim","Miami");
+
+        List<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+
+        System.out.println(students);
+
+        Collections.sort(students);
+
+    }
+
+}
