17e06b1f84772739cc81d9faeb0484201b304cf4
alexKamo <sashakamor@gmail.com>
2024-07-18 20:34:04 -0500

    JavaCourses

--- src/designPaterns1/InfoParserProxy.java
+++ src/designPaterns1/InfoParserProxy.java
@@ -1,8 +1,11 @@
 package designPaterns1;
 
 public class InfoParserProxy implements InfoParserInterface{
+
+    InfoParser infoParser;
     @Override
     public void readData() {
-
+        if (infoParser == null) infoParser = new InfoParser();
+        infoParser.readData();
     }
 }
--- src/designPaterns1/MainClient.java
+++ src/designPaterns1/MainClient.java
@@ -4,8 +4,8 @@ public class MainClient {
 
     public static void main(String[] args) {
 
-        InfoParser infoParser = new InfoParser();
-
+        InfoParserProxy infoParserProxy = new InfoParserProxy();
+        infoParserProxy.readData();
     }
 
 }
