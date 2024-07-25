265fdd55c7fe05fe05f6f5ed7540981b4acec5e4
alexKamo <sashakamor@gmail.com>
2024-07-25 17:21:12 -0500

    JavaCourses

--- src/TESTS/BankAccount.java
+++ src/TESTS/BankAccount.java
@@ -13,8 +13,17 @@ public class BankAccount {
         BankAccount account1 = new BankAccount();
 
         DepositTask task = new DepositTask(account1,100);
+        DepositTask task2 = new DepositTask(account1,200);
+        DepositTask task3 = new DepositTask(account1,300);
+        DepositTask task4 = new DepositTask(account1,400);
+        DepositTask task5 = new DepositTask(account1,500);
 
-        Thread thread1 = new Thread(account1);
+
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task2);
+        Thread thread3 = new Thread(task3);
+        Thread thread4 = new Thread(task4);
+        Thread thread5 = new Thread(task5);
 
 
 
