ba2e6f9552b4644a0867f15d24821f7af5e0e3d1
alexKamo <sashakamor@gmail.com>
2024-09-18 16:39:55 -0500

    JavaCourses

--- src/DATES/Task.java
+++ src/DATES/Task.java
@@ -4,7 +4,10 @@ public class Task {
 
     public static void main(String[] args) {
 
+        String[] checkInAndCheckOut={"John Smith CI:24/02/21 CO:24/02/22",  "Patel Harsh CI:24/03/10 CO:24/10/24" ,
+                "Kim Kardash CI:23/05/11 CO:24/06/19"};
 
+        // Find out total month and days hotel had guest
 
     }
 
