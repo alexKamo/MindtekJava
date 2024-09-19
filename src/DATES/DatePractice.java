c771ee9de396b978d5bbb29e04cef02447b55f8f
alexKamo <sashakamor@gmail.com>
2024-09-19 15:08:08 -0500

    JavaCourses

---
+++ src/DATES/DatePractice.java
@@ -0,0 +1,15 @@
+package DATES;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+public class DatePractice {
+
+    public static void main(String[] args) {
+
+        LocalDateTime localDateTime = LocalDateTime.of(2024, Month.MAY, 17, 05, 10,0);
+        System.out.println(localDateTime);
+
+    }
+
+}
