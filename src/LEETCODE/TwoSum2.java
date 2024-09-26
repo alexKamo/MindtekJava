6f32bb551a8167065fe645fff6791cd23c259860
alexKamo <sashakamor@gmail.com>
2024-09-26 18:46:09 -0500

    JavaCourses

---
+++ src/LEETCODE/TwoSum2.java
@@ -0,0 +1,36 @@
+package LEETCODE;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum2 {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (map.containsKey(target-nums[i])){
+        //         return new  int[]{i,map.get(target-nums[i])};
+        //     }else {
+        //         map.put(nums[i],i);
+        //     }
+        // }
+        // return null;
+
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i+1; j < nums.length; j++) {
+
+                if (nums[i]+nums[j] == target){
+                    return new int[]{i,j};
+                }
+
+            }
+
+        }
+        return null;
+
+    }
+
+}
