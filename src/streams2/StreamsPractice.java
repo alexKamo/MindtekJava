478eedf83e0649d5f098d653d66116b3d18c69f5
alexKamo <sashakamor@gmail.com>
2024-09-28 10:22:08 -0500

    JavaCourses

---
+++ src/streams2/StreamsPractice.java
@@ -0,0 +1,30 @@
+package streams2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsPractice {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>(List.of("Apple","Banana","Orange","Kiwi","Melon"));
+        // Get oranges that contains letter 'a'
+
+        list.stream()
+                .filter(fruit -> fruit.toLowerCase().contains("a"))
+                .forEach(fruit -> System.out.println(fruit));
+
+        //Method Reference
+        // System.out::println   OR   x -> System.out.println(x);
+
+
+        // Get lengths of each fruit as a list.
+
+        List<Integer> length = list.stream()
+                .map(x -> x.length()) //String -> int
+                .collect(Collectors.toList());
+        System.out.println(length);
+    }
+
+}
