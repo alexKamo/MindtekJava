c6f75f8cec433226144e036014046e31fad43bf1
alexKamo <sashakamor@gmail.com>
2024-10-03 18:32:30 -0500

    JavaCourses

---
+++ src/LEETCODE/RankTransformOfAnArray.java
@@ -0,0 +1,19 @@
+package LEETCODE;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class RankTransformOfAnArray {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] temp = arr.clone();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Arrays.sort(temp);
+        for ( int t : temp){
+            map.putIfAbsent(t, map.size() + 1);
+        }
+        for (int i = 0; i < arr.length; i++){
+            temp[i] = map.get(arr[ i]);
+        }
+        return temp;
+    }
+}
