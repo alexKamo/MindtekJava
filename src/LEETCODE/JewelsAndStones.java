f5dc846d94de42269f452ce1ea69d8ee52d88b80
alexKamo <sashakamor@gmail.com>
2024-10-10 18:35:55 -0500

    JavaCourses

--- src/LEETCODE/JewelsAndStones.java
+++ src/LEETCODE/JewelsAndStones.java
@@ -9,15 +9,24 @@ public class JewelsAndStones {
 
     public int numJewelsInStones(String jewels, String stones) {
 
+//        int res = 0;
+//
+//        for (char el : stones.toCharArray()) if (jewels.contains(el+""))
+//            res++;
+//        return res;
+
         int res = 0;
 
-        for (char el : stones.toCharArray()){
-             if (jewels.contains(el+"")){
-                 res++;
-             }
+        for(int i = 0; i < stones.length(); i++) {
+            for(int j = 0; j < jewels.length(); j++) {
+                if(stones.charAt(i) == jewels.charAt(j)){
+                    res++;
+                }
+            }
         }
 
         return res;
+
     }
 
 }
