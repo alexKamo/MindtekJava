9578ce406b85b85df246ecf5bec5286be83aab91
alexKamo <sashakamor@gmail.com>
2024-10-10 18:20:08 -0500

    JavaCourses

--- src/LEETCODE/JewelsAndStones.java
+++ src/LEETCODE/JewelsAndStones.java
@@ -1,11 +1,17 @@
 package LEETCODE;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class JewelsAndStones {
 
     public int numJewelsInStones(String jewels, String stones) {
 
         int i = 0;
-        for(String s : stones){
+
+        List<String> list = Arrays.asList(stones.split(" "));
+
+        for(String s : list){
             if (jewels.contains(s)){
                 i++;
             }
