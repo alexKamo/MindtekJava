b04d823014d9970b8677fa65f8756bf6d8d70c04
alexKamo <sashakamor@gmail.com>
2024-11-16 11:24:15 -0600

    JavaCourses

--- src/LEETCODE/DeleteCharactersToMakeFancyString.java
+++ src/LEETCODE/DeleteCharactersToMakeFancyString.java
@@ -3,23 +3,21 @@ package LEETCODE;
 public class DeleteCharactersToMakeFancyString {
 
     public static String makeFancyString(String s){
-        s = "baaaaaaaaaabbbaa";
         StringBuilder res = new StringBuilder();
         res.append(s.charAt(0));
         int counter = 1;
         for (int i = 1; i < s.length(); i++) {
-            char letter = s.charAt(i);
-            if (res.charAt(res.length()-1)==letter){
+            if (res.charAt(res.length()-1)==s.charAt(i)){
                 if (counter == 2) continue;;
-                res.append(letter);
+                res.append(s.charAt(i));
                 counter++;
             }else{
-                res.append(letter);
+                res.append(s.charAt(i));
                 counter = 1;
             }
         }
 
-        return "baabbaa";
+        return res.toString();
     }
 
 }
